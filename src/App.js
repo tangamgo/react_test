4446caeed9aee0178bcc0a21bbebdf4965de7256
tangamgo <dbals8555@naver.com>
2022-11-14 20:25:49 +0900

    cleanup test

--- src/App.js
+++ src/App.js
@@ -2,30 +2,21 @@
 //import styles from "./App.module.css";
 import {useState, useEffect} from "react";
 
+function Hello() {
+    useEffect(() => {
+        console.log("create :)")
+        return () => console.log("bye :(");
+    }, [])
+    return <h1>Hello</h1>;
+}
+
 function App() {
-    const [counter, setValue] = useState(0);
-    const [keyword, setKeyword] = useState("");
-    const onClick = () => setValue((prev) => prev + 1);
-    const onChange = (event) => {
-        setKeyword(event.target.value)
-    };
-    useEffect(() => {
-        console.log("i run only onec")
-    }, []);
-    useEffect(() => {
-        console.log("i run when 'keyword' changes ");
-    }, [keyword])
-    useEffect(() => {
-        console.log("i run when 'counter' changes ");
-    }, [counter])
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
     return (
         <div>
-            <input value={keyword}
-                   onChange={onChange}
-                   type={"text"}
-                   placeholder={"Search here..."}/>
-            <h1> {counter} </h1>
-            <button onClick={onClick}>click me</button>
+            {showing ? <Hello/> : null}
+            <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
         </div>
     );
 }
