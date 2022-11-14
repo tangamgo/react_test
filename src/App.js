09f15e5cec27f1d5c5f6407c8024bf243f1d6a01
tangamgo <dbals8555@naver.com>
2022-11-15 00:54:09 +0900

    movie app test 2

--- src/App.js
+++ src/App.js
@@ -1,44 +1,21 @@
-//import Button from "./Button"
-//import styles from "./App.module.css";
-import {useState, useEffect} from "react";
+// import {useState, useEffect} from "react";
 
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const getMovies = async () => {
-        const json = await (
-            await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
-            )
-        ).json();
-        setMovies(json.data.movies);
-        setLoading(false);
-    }
-    useEffect(() => {
-        getMovies()
-    }, [])
-    console.log(movies);
     return (
-        <div>
-            {loading ? (
-                <strong>Loading...</strong>
-            ) : (
-                <div>{movies.map(movie =>
-                    <div key={movie.id}>
-                        <img src={movie.medium_cover_image}/>
-                        <h2>{movie.title}</h2>
-                        <p>{movie.summary}</p>
-                        <ul>
-                            <li>
-                                {movie.genres.map(g =>
-                                    <li key={g}>{g}</li>
-                                )}
-                            </li>
-                        </ul>
-                    </div>)}
-                </div>
-            )}
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/movie" element={<Detail/>}/>
+            </Routes>
+        </Router>
     );
 }
 
